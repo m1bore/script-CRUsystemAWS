201cdf680d58e02fc8fb5c5039c4b4e861f2e3e5
m1bore <122971250+m1bore@users.noreply.github.com>
2023-11-24 17:04:59 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Variables
+vpc_name="mileserik"
+region="us-east-1"
+cidr_block="192.168.1.0/22"
+
+# Crear VPC
+vpc_id=$(aws ec2 create-vpc --cidr-block $cidr_block --query 'Vpc.VpcId' --output text --region $region)
+aws ec2 create-tags --resources $vpc_id --tags Key=Name,Value=$vpc_name --region $region
+aws ec2 modify-vpc-attribute --vpc-id $vpc_id --enable-dns-support "{\"Value\":true}" --region $region
+aws ec2 modify-vpc-attribute --vpc-id $vpc_id --enable-dns-hostnames "{\"Value\":true}" --region $region
+echo "VPC creada con ID: $vpc_id"
+
+# Crear subredes y EC2 para cada departamento
+declare -A departments=(
+    [ingenieria]=100
+    [desarrollo]=500
+    [mantenimiento]=20
+    [soporte]=250
+)
+
+subnet_index=1
+for department in "${!departments[@]}"; do
+    # Crear subred
+    subnet_cidr="192.168.$subnet_index.0/24"
+    subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block $subnet_cidr --availability-zone $regiona --query 'Subnet.SubnetId' --output text --region $region)
+    
+    # Crear EC2
+    instance_name="ec2-$department"
+    instance_count=${departments[$department]}
+    for ((i=1; i<=$instance_count; i++)); do
+        aws ec2 run-instances --image-id ami-xxxxxxxxxxxxx --instance-type t2.micro --subnet-id $subnet_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance_name-$i}]" --region $region
+    done
+
+    echo "Subred y EC2 para $department creadas"
+    ((subnet_index++))
+done
+
+echo "Script completado"
